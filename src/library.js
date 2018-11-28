3d466234a2be4eb8e23e44a8e1fe0c1e5afdff33
Nakka Leelaprasanth <44019459+leelanakka@users.noreply.github.com>
2018-11-28 09:52:27 +0530

    merging

--- src/library.js
+++ src/library.js
@@ -2,8 +2,55 @@ const fillConsecutiveNumbersArray = function(limit){
   let array = [];
   for(let index = 0; index < limit ; index++){
     array.push(index+1);
-  }
+  };
   return array;
-}
+};
 
-module.exports = { fillConsecutiveNumbersArray }
+const createArray = function(length,filler){
+  return new Array(length).fill(filler);
+};
+
+const dashline = function(length){
+  if(length==0){
+    return "";
+  };
+  return createArray(length*4+1,"-").join("");
+};
+
+const printBoard = function(array){
+  let board = [];
+  let length = array[0].length;
+  board.push(dashline(length));
+  for(let row = 0;row < length;row++){
+    board = board.concat(createRow(array[row]));
+  };
+  return board;
+};
+
+const createRow = function(array){
+  let row = [];
+  if(array.length == 0){
+    return array;
+  }
+  let column = array.map(addSpaces).join("|");
+  let cell = "|"+column+"|";
+  row.push(cell);
+  row.push(dashline(array.length));
+  return row;
+};
+
+const addSpaces = function(text){
+  if(text != undefined){
+    return " "+text+" ";
+  };
+  return "";
+};
+
+module.exports = { 
+  fillConsecutiveNumbersArray,
+  printBoard,
+  addSpaces,
+  createArray,
+  dashline,
+  createRow
+};
--- test/libTest.js
+++ test/libTest.js
@@ -1,5 +1,12 @@
 const {deepEqual, equal} = require('assert');
-const {fillConsecutiveNumbersArray} = require('../src/library.js'); 
+const {
+  fillConsecutiveNumbersArray,
+  printBoard,
+  addSpaces,
+  createArray,
+  dashline,
+  createRow
+} = require('../src/library.js'); 
 
 describe('fillConsecutiveNumbersArray', () => {
   it('should give empty array while passing zero', () => {
@@ -8,5 +15,76 @@ describe('fillConsecutiveNumbersArray', () => {
 
   it('should give with list of consecutive numbers according to the size', () => {
     deepEqual(fillConsecutiveNumbersArray(3),[1,2,3]);
+  });
+});
+
+describe('printBoard', () => {
+  it('should return empty board', () => {
+    deepEqual(printBoard([[]]),[""]);
+  });
+
+  it('should return board with size 2*2 if input is array containing 2 arrays', () => {
+    let expectedOutput = [ '---------', 
+      '| 1 | 2 |', 
+      '---------', 
+      '| 3 | 4 |', 
+      '---------' ];
+    deepEqual(printBoard([[1,2],[3,4]]),expectedOutput);
+  });
+
+  it('should return board with size 4*4 if input is array containing 2 arrays', () => {
+    let expectedOutput = [ '-----------------',
+      '| 1 | 2 | 3 | 4 |',
+      '-----------------',
+      '| 4 | 5 | 6 | 7 |',
+      '-----------------',
+      '| 8 | 9 | 0 | 1 |',
+      '-----------------',
+      '| 2 | 3 | 4 | 5 |',
+      '-----------------' ];
+    deepEqual(printBoard([[1,2,3,4],[4,5,6,7],[8,9,0,1],[2,3,4,5]]),expectedOutput);
+  });
+});
+
+describe('addSpaces',()=>{
+  it('should add a space before and after the text',()=>{
+    equal(addSpaces("game")," game ");
+  });
+  it('should return empty if you dont pass anything',()=>{
+    equal(addSpaces(),"");
+  });
+});
+
+describe('createArray',()=>{
+  it('should return empty array for 0 as input',()=>{
+    deepEqual(createArray(0,0),[]);
+  });
+  it('should return array for given size and fill that as per input',()=>{
+    deepEqual(createArray(1,0),[0]);
+  });
+  it('should return array for given size and fill that as per input',()=>{
+    deepEqual(createArray(2,"a"),["a","a"]);
+  });
+});
+
+describe('dashline',()=>{
+  it('should return nothing if the input is 0',()=>{
+    equal(dashline(0),"");
+  });
+  it('should return dashes as per the given input',()=>{
+    equal(dashline(1),"-----");
+    equal(dashline(2),"---------");
+    equal(dashline(3),"-------------");
+  });
+});
+
+describe('createRow',()=>{
+  it('should return row as per given array and current postion',()=>{
+    deepEqual(createRow([1,2,3]),['| 1 | 2 | 3 |','-------------']);
+    deepEqual(createRow([2,3]),[ '| 2 | 3 |', '---------' ]);
+    deepEqual(createRow([3]),[ '| 3 |', '-----' ]);
+  });
+  it('should return empty if user passes empty',()=>{
+    deepEqual(createRow([]),[]);
   });
 });
